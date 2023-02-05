2121445a800ff028283fa774dac762196bba79d8
visinh0 <dmitriibd90@gmail.com>
2023-02-05 18:14:49 +0200

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgres://db_60oh_user:mcArRNxWaJQABqmOe2EddC1raA3xQ9Pa@dpg-cffs1ipgp3jjse9u6l60-a.frankfurt-postgres.render.com/db_60oh',
     port : 5432,
     user : 'postgres',
     password : 'test',
